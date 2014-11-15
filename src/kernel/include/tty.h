018e683a7256b24fbd678c930bb983d924e1a6d1
MihaiPro <anonimatanonimus@gmail.com>
2014-11-15 13:39:36 +0200

    Basic kernel. Print function.

---
+++ src/kernel/include/tty.h
@@ -0,0 +1,16 @@
+#if !defined(__cplusplus)
+#include <stdbool.h> /* C doesn't have booleans by default. */
+#endif
+#include <stddef.h>
+#include <stdint.h>
+
+size_t terminal_row;
+size_t terminal_column;
+uint8_t terminal_color;
+uint16_t* terminal_buffer;
+ 
+void terminal_initialize(); 
+void terminal_setcolor(uint8_t color);
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y);
+void terminal_putchar(char c);
+void terminal_writestring(const char* data);
